d958bae74a3cf175122c5450aca975cc38c7158c
tjfitzzster <tjfitzster@gmail.com>
2021-10-27 16:13:46 +0100

    Creating a new layout for the register and login screen.

--- ChatApplication/app/src/main/java/org/wit/chatapplication/MainActivity.kt
+++ ChatApplication/app/src/main/java/org/wit/chatapplication/MainActivity.kt
@@ -2,13 +2,27 @@ package org.wit.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber.i
 import android.widget.Toast
+import org.wit.chatapplication.databinding.LoginscreenBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: LoginscreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = LoginscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+
+        binding.loginbutton.setOnClickListener() {
+            Toast.makeText(this, "LOGIN BUTTON PRESSED", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.registerbutton.setOnClickListener() {
+            Toast.makeText(this, "REGISTER BUTTON PRESSED", Toast.LENGTH_SHORT).show()
+        }
     }
 }
