46fdbeb1077ab30492d25954eef70b4f7a526410
tjfitzzster <tjfitzster@gmail.com>
2021-10-28 12:18:34 +0100

    Data Validation Complete.

--- ChatApplication/app/src/main/java/org/wit/chatapplication/MainActivity.kt
+++ ChatApplication/app/src/main/java/org/wit/chatapplication/MainActivity.kt
@@ -2,11 +2,11 @@ package org.wit.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import timber.log.Timber.i
 import android.widget.Toast
 import org.wit.chatapplication.databinding.LoginscreenBinding
 import org.wit.chatapplication.models.UserModel
 import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,36 +19,51 @@ class MainActivity : AppCompatActivity() {
         binding = LoginscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Toast.makeText(this, "Welcome123", Toast.LENGTH_SHORT).show()
-        Timber.plant(Timber.DebugTree())
+         //   Toast.makeText(this, "Welcome123", Toast.LENGTH_SHORT).show()
 
         binding.loginbutton.setOnClickListener() {
 
-             user.emailaddress = binding.editTextTextEmailAddress2.text.toString()
+            user.emailaddress = binding.editTextTextEmailAddress2.text.toString()
             user.password = binding.editTextTextPassword.text.toString()
 
-            if (user.emailaddress.isNotEmpty() && user.password.isNotEmpty()) {
+            if(users.size > 0){
+                // More than one user
+
                 for (i in users.indices) {
-                    if (this.users[i].emailaddress == user.emailaddress)
+                    if (this.users[i].emailaddress.equals(user.emailaddress))
                     {
-                            // email address correct.
-                        if (this.users[i].password == user.password)
+                        // email address correct
+
+                        if (this.users[i].password.equals(user.password))
                         {
-                            // email address correct bring them to chat activity
+                            // PASSWROD correct
+                            Toast.makeText(this, "PASSWORD CORRECT", Toast.LENGTH_SHORT).show()
                         }
-                        else Toast.makeText(this, "PASSWORD INCORRECT", Toast.LENGTH_SHORT).show()
+                        else{
+                            Toast.makeText(this, "PASSWORD INCORRECT", Toast.LENGTH_SHORT).show()
+                        }
                     }
-                    else Toast.makeText(this, "NO USER WITH THAT ADDRESS", Toast.LENGTH_SHORT).show()
-                 //  i("Users[$i]:${this.users[i]}")
-                 //   users.add(user.copy())
+                    else{
+                        Toast.makeText(this, "NO ONE IS REGISTERED WITH THAT EMAIL ADDRESS", Toast.LENGTH_SHORT).show()
+                    }
+
                 }
-
             }
-
+            else{
+                //YOU MUST CREATE A NEW USER
+                Toast.makeText(this, "PLEASE REGISTER A USER", Toast.LENGTH_SHORT).show()
+            }
         }
 
+
         binding.registerbutton.setOnClickListener() {
-            Toast.makeText(this, "$user.password", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "REGISTER BUTTON PRESSED", Toast.LENGTH_SHORT).show()
+
+            if ((user.emailaddress.isNotEmpty()) && (user.password.isNotEmpty())) {
+                users.add(user.copy())
+                Toast.makeText(this, "REGISTERING A NEW USER", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
